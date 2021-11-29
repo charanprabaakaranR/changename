13666d793960f39fe0018c79ddf3ff7df59640d7
charanprabaakaranR <charan@sostech.in>
2021-11-29 23:59:13 +0530

    updated the names

--- src/component/Name.js
+++ src/component/Name.js
@@ -4,14 +4,14 @@ import SecondChild from './SecondChild';
 export default class Name extends Component {
     constructor(){
         super();
-        this.state = {name1 : 'tida',name2 : 'kito'}
+        this.state = {name1 : 'sharvappu',name2 : 'charan'}
     }
     render() {
         return (
             <div>
                 <h1>
                     <SecondChild name1 = {this.state.name1} name2 = {this.state.name2}/>
-                    <SecondChild name1 = '123' name2 = '456' />
+                    <SecondChild name1 = 'ganesh' name2 = 'tida' />
                 </h1>
             </div>
         )
